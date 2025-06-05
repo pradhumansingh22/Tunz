5b72ea639232a4ed18162ae8794ca512245c21fc
prahumansingh22 <pradhuman362@gmail.com>
2025-06-05 22:28:32 +0530

    fixed build errors

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { OpenSans } from "./lib/fonts";
 import { getServerSession } from "next-auth";
 import { JoinButton } from "./components/JoinButton";
 import { Featured } from "./components/featured";
-import { useEffect } from "react";
-import { useCurrentSongQueue } from "./lib/store/myStore";
 
 export default async function HomePage() {
   const session = await getServerSession();
